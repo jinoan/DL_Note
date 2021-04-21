cc06dcf2ebb5cc55ea4973db9f29bc7143b11934
Jinho Ahn <jinoan89@gmail.com>
2021-04-21 18:50:09 +0900

    feat: move set_random_seed to utils

--- preprocessing/ready.py
+++ preprocessing/ready.py
@@ -1,15 +1,6 @@
-import os
-import random
-import numpy as np
 import pathlib
 from sklearn.model_selection import StratifiedKFold
 from ..utils.chart import stacked_bar
-
-def set_random_seed(seed_num:int=None):
-    if seed_num:
-        os.environ["PYTHONHASHSEED"] = str(seed_num)
-        random.seed(seed_num)
-        np.random.seed(seed_num)
 
 def get_dataset(ds_path:str, suffixes:str=None):
     ds_path = pathlib.Path(ds_path)
@@ -51,24 +42,3 @@ def fold_dataset_info(fold_ds):
     for fold, train_data_size, test_data_size in zip(kwargs["x"], *kwargs["y"]):
         print(f"[fold {fold}] train_data_size: {train_data_size}, test_data_size: {test_data_size}")
     stacked_bar(**kwargs)
-
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--path", "-p", type=str,
-                        help="Dataset path. This path should be in 'datasets/'")
-    parser.add_argument("--suffixes", "-s", type=str, nargs="+",
-                        help="Extracting data containing specific suffixes. e.g. --suffixes .jpg .png")
-    parser.add_argument("--folds", "-f", type=int)
-    parser.add_argument("--random_seed", "-r", type=int, default=None)
-    args = parser.parse_args()
-    set_random_seed(args.random_seed)
-    images, labels = get_dataset(args.path, args.suffixes)
-    if args.folds:
-        folds = fold_dataset(images, labels, 5)
-        info_folded_dataset(folds)
-        for train_images, train_labels, test_images, test_labels in folds:
-            for train_image in train_images:
-                print(train_image)
