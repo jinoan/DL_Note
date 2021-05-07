2b59a10545171604a781b567404ca552a547720e
Jinho Ahn <jinoan89@gmail.com>
2021-05-07 18:40:21 +0900

    feat: classification을 하위 class로 분리

--- cv/__init__.py
+++ cv/__init__.py
@@ -2,45 +2,29 @@
 import os
 from pathlib import Path
 import numpy as np
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
 import cv2
-from typing import Union
+from torch.utils.data import Dataset
+from typing import Union, List
 from .utils.chart import stacked_bar
 
-class TorchDataset:
-    def __init__(self,
-                 data_path:str=None,
-                 suffixes:str=None,
-                 image_size:tuple=(256, 256),
-                 color_mode:Union[int, str]=1,  # 1: 'color', '0': 'grey', '-1': 'unchanged'
-                 augmentations=None,
-                 transforms=None,
-                 **kwargs):
+class TorchDataset(Dataset):
+    def __init__(
+        self,
+        images=None,
+        labels=None,
+        classes=None,
+        image_size=(256, 256),
+        color_mode=1,  # 1: 'color', '0': 'grey', '-1': 'unchanged'
+        augmentations=None,
+        transforms=None,
+        **kwargs
+    ):
         super(TorchDataset, self).__init__()
 
-        self.images = None
-        self.labels = None
-        self.classes = None
-
-        if data_path is not None:
-            path = Path(data_path)
-            assert path.parent.parts[-1] == "datasets", "the dataset dir path should be in 'datasets/'"
-
-            def _extract_suffix(path):
-                if suffixes is None or path.suffix in suffixes:
-                    return str(path)
-
-            self.images = list(filter(None, map(_extract_suffix, path.rglob("*/*"))))
-            self.classes = os.listdir(path)
-            sparse_encode = {}
-            for i, c in enumerate(self.classes):
-                sparse_encode[c] = i
-            self.labels = list(map(lambda data: sparse_encode[data.split("/")[-2]], self.images))
-
-        if "images" in kwargs.keys(): self.images = kwargs["images"]
-        if "labels" in kwargs.keys(): self.labels = kwargs["labels"]
-        if "classes" in kwargs.keys(): self.classes = kwargs["classes"]
-
+        self.images = images
+        self.labels = labels
+        self.classes = classes
         self.image_size = image_size
         self.color_mode = {1: "color", 0: "grey", -1:"unchanged"}.get(color_mode, color_mode)
         self.augmentatinos = augmentations
@@ -60,25 +44,16 @@ class TorchDataset:
         image = (image/255).astype("float")
         if image.ndim == 2:
             image = image[..., np.newaxis]
-            
+
+        label = list(map(lambda x: int(x==self.labels[index]), self.classes))  # one-hot encoding
+
         if self.transforms is not None:
-            label = self.transforms(self.labels[index])
+            image, label = self.transforms(image, label)
         
-        return 
-        
-        """
-        필요한 작업:
-        augmentation 적용
-        transforms 적용
-        augmentation 객체 받아오는 기능 추가
-        """
+        return image, label
 
-    def config(self, **kwargs):
-        self.image_size = kwargs.get("image_size", (256, 256))
-        self.grey_scale = kwargs.get("grey_scale", False)
-
-    def fold(self, num_folds, info=False):
-        skf = StratifiedKFold(n_splits=num_folds, shuffle=True).split(self.images, self.labels)
+    def fold(self, num_folds=5, info=False):
+        kf = KFold(n_splits=num_folds, shuffle=True).split(self.images, self.labels)
         _get_images = lambda idx: self.images[idx]
         _get_labels = lambda idx: self.labels[idx]
         fold_ds = list(map(lambda x: (
@@ -88,21 +63,47 @@ class TorchDataset:
             TorchDataset(images=list(map(_get_images, x[1])),
                          labels=list(map(_get_labels, x[1])),
                          classes=self.classes) # test dataset
-        ),skf))
+        ), kf))
         if info:
             self.__fold_dataset_info(fold_ds)
         return fold_ds
 
-    def __fold_dataset_info(self, fold_ds):
-        kwargs = {"x": [], "y": [[], []]}
-        for i, (train_ds, test_ds) in enumerate(fold_ds):
-            kwargs["x"].append(i+1)
-            train_data_size = 0
-            test_data_size = 0
-            for _ in train_ds.images: train_data_size += 1
-            for _ in test_ds.images: test_data_size += 1
-            kwargs["y"][0].append(train_data_size)
-            kwargs["y"][1].append(test_data_size)
-        for fold, train_data_size, test_data_size in zip(kwargs["x"], *kwargs["y"]):
-            print(f"[fold {fold}] train_data_size: {train_data_size}, test_data_size: {test_data_size}")
+    def __fold_dataset_info(self, fold_ds, **kwargs):
+        if fold_ds:
+            kwargs = {"x": [], "y": [[], []]}
+            for i, (train_ds, test_ds) in enumerate(fold_ds):
+                kwargs["x"].append(i+1)
+                train_data_size = 0
+                test_data_size = 0
+                for _ in train_ds.images: train_data_size += 1
+                for _ in test_ds.images: test_data_size += 1
+                kwargs["y"][0].append(train_data_size)
+                kwargs["y"][1].append(test_data_size)
+            for fold, train_data_size, test_data_size in zip(kwargs["x"], *kwargs["y"]):
+                print(f"[fold {fold}] train_data_size: {train_data_size}, test_data_size: {test_data_size}")
         stacked_bar(**kwargs)
+
+class TorchClassificationDataset(TorchDataset):
+    def __init__(
+        self,
+        data_path=None,
+        suffixes=None,
+        *args,
+        **kwargs
+    ):
+        super(TorchClassificationDataset, self).__init__(*args, **kwargs)
+
+        if data_path is not None:
+            path = Path(data_path)
+            assert path.parent.parts[-1] == "datasets", "The dataset dir path should be in 'datasets/'."
+
+            def _extract_suffix(path):
+                if suffixes is None or path.suffix in suffixes:
+                    return str(path)
+
+            self.images = list(filter(None, map(_extract_suffix, path.rglob("*/*"))))
+            self.classes = os.listdir(path)
+            sparse_encode = {}
+            for i, c in enumerate(self.classes):
+                sparse_encode[c] = i
+            self.labels = list(map(lambda data: sparse_encode[data.split("/")[-2]], self.images))
